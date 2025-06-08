f3594ccf42bcef2d061fcc862e6e0caf73a3fb4d
lzh <2661006892@qq.com>
2025-06-08 22:12:36 +0800

    添加TCP客户端示例

---
+++ wifi-iot/app/tcp_client/demo.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "ohos_init.h"
+#include "ohos_types.h"
+#include "cmsis_os2.h"
+#include "wifi_connecter.h"
+#include "tcp_client.h"
+
+/* WiFi信息 */
+// 账号
+#define  HOTSPOT_SSID           "OpenHarmony"
+// 密码 
+#define  HOTSPOT_PASSWD         "123456789"
+// 加密方式 
+#define  HOTSPOT_TYPE           WIFI_SEC_TYPE_PSK
+// TCP服务器IP地址
+#define PARAM_SERVER_ADDR "192.168.10.117"
+// 用于标识TCP服务器端口
+#define PARAM_SERVER_PORT 5678
+
+
+static void TcpClientTask(void *arg)
+{
+    (void)arg;
+    /* 初始化WIFI参数 */
+    WifiDeviceConfig apConfig = {
+        // 热点名称
+        .ssid = HOTSPOT_SSID,
+        // 热点密码
+        .preSharedKey = HOTSPOT_PASSWD,
+        // 加密方式(PSK)
+        .securityType = HOTSPOT_TYPE,
+    };
+
+    /* 连接WIFI */
+    int netId = ConnectToHotspot(&apConfig);
+    if (netId < 0) {
+        LOG_ERROR("Connect to AP failed!");
+    }
+
+    TcpClientDemo(PARAM_SERVER_ADDR, PARAM_SERVER_PORT);
+
+    LOG_INFO("disconnect ap ...");
+    DisconnectWithHotspot(netId);
+    LOG_INFO("disconnect ap done");
+
+}
+
+static void TcpClientEntry(void)
+{
+    osThreadAttr_t attr = {
+        .name = "TcpClientTask",
+        .stack_size = 10240,
+        .priority = osPriorityNormal,
+    };
+    osThreadId_t thread_id = osThreadNew(TcpClientTask, NULL, &attr);
+    if (thread_id == NULL) {
+        printf("[Thread Create] osThreadNew(%s) failed.\r\n", "TcpClientTask");
+    } else{
+        printf("[Thread Create] osThreadNew(%s) success, thread id: %d.\r\n", "TcpClientTask", thread_id);
+    }
+}
+
+SYS_RUN(TcpClientEntry);
---
+++ wifi-iot/app/tcp_client/tcp_client.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include "ohos_init.h"
+#include "ohos_types.h"
+#include "cmsis_os2.h"
+#include "lwip/sockets.h"
+#include "lwip/def.h"
+#include "tcp_client.h"
+
+
+/* 缓冲区 */
+static char request[] = "hello";
+static char response[128] = "";
+
+
+/**
+ * @brief TCP Client测试函数
+ * 
+ * lwip的API分两种，一中是带"_lwip"后缀的，一种则是不带后缀的。
+ * 个人建议使用带后缀的，代码比较有区分度
+ * 
+ */
+void TcpClientDemo(const char *host, unsigned short port)
+{
+    // 返回值，发送/接收的字节数
+    ssize_t retval = 0;
+
+    // 创建一个 TCP Socket(网络套接字)，返回文件描述符
+    int sockfd = lwip_socket(AF_INET, SOCK_STREAM, 0);
+
+    // 设置服务端的地址信息，包括协议、端口、IP地址等
+    struct sockaddr_in serverAddr = {
+        // 选择IPv4协议
+        .sin_family = AF_INET,
+        // 端口号，使用htons函数从主机字节序转为网络字节序
+        // 大端序是网络标准字节序（建议转换保持兼容性）
+        .sin_port = lwip_htons(port),
+    };
+
+    // 将服务端IP地址从“点分十进制”字符串，转化为标准格式（32位整数）
+    if (lwip_inet_pton(AF_INET, host, &serverAddr.sin_addr) <= 0) {
+        LOG_ERROR("lwip_inet_pton fail!");
+        // 跳转到cleanup部分，主要是关闭连接
+        goto do_cleanup;
+    }
+
+    // 尝试和目标主机建立连接，连接成功会返回0，失败返回-1
+    if (lwip_connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+        LOG_ERROR("connect fail!");
+        // 跳转到cleanup部分，主要是关闭连接
+        goto do_cleanup;
+    }
+    // 建立连接成功之后，sockfd就具有了“连接状态”，
+    // 后续的发送和接收，都是针对指定的目标主机和端口
+    LOG_INFO("connect server %s success!", host);
+
+    // 使用默认阻塞模式发送数据
+    retval = lwip_send(sockfd, request, sizeof(request), 0);
+    // 对方的通信端关闭时，返回值为0；返回值小于0表示接收失败
+    if (retval <= 0 ) {
+        LOG_ERROR("send fail, %ld!", retval);
+        goto do_cleanup;
+    }
+    LOG_INFO("send request {%s} to server %ld", request, retval);
+
+    // 使用默认阻塞模式接收数据
+    retval = lwip_recv(sockfd, response, sizeof(response), 0);
+    // 对方的通信端关闭时，返回值为0；返回值小于0表示接收失败
+    if (retval <= 0 ) {
+        LOG_ERROR("recv fail, %ld!", retval);
+        goto do_cleanup;
+    }
+    // 在末尾添加字符串结束符'\0'，以便后续的字符串操作
+    response[retval] = '\0';
+    LOG_INFO("send response {%s} to server %ld", response, retval);
+
+    // 接收数据
+do_cleanup:
+    LOG_WARN("do_cleanup...");
+    // 关闭Socket
+    lwip_close(sockfd);
+}
+
